5128c45ed394dab1019ce45e55f7da0ec75e46c4
Juan Cruz Rodriguez <siwidjc@gmail.com>
2023-03-20 21:28:36 -0300

    Primer desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
